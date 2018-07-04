fa9f974f38dd29942bd3cb785eb683d538b8ba08
Lanesra712 <danvic96@hotmail.com>
2018-07-04 18:14:20 +0800

    Use swagger to description api

--- Danvic.Vu/Danvic.Vu/Startup.cs
+++ Danvic.Vu/Danvic.Vu/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Danvic.Vu
 {
@@ -23,6 +18,20 @@ namespace Danvic.Vu
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //register the swagger document
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info
+            {
+                Version = "v1",
+                Title = "Vu Project API",
+                Description = "A front-end separation project using ASP.NET Core Web API",
+                Contact = new Contact
+                {
+                    Name = "Danvic Wang",
+                    Email = "danvic96@hotmail.com",
+                    Url = "https://github.com/Lanesra712"
+                }
+            }));
+
             services.AddMvc();
         }
 
@@ -33,6 +42,26 @@ namespace Danvic.Vu
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //use static files
+            app.UseStaticFiles();
+
+            //Use Swagger to description API 
+            app.UseSwagger();
+
+            //Enable middleware to show swagger ui
+            /**
+             *When start appliaction and go to http://localhost:<port>/swagger/v1/swagger.json
+             * the description information will show in the swagger.json
+             */
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vu API V1.0");
+
+                //let the api description page in the app's root http://localhost:<port>
+                //the default description page in http://localhost:<port>/swagger 
+                //c.RoutePrefix = string.Empty;
+            });
 
             app.UseMvc();
         }
